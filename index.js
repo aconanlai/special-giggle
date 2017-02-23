9350a75b3b6b098a5bce901a17ea6dd53a8d655f
Conan <aconanlai@gmail.com>
2017-02-22 21:21:44 -0500

    google sheets api

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const GoogleSpreadsheet = require('google-sheets-node-api');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('eh');
+const creds = require('./credentials.json');
+
+const mySheet = new GoogleSpreadsheet('10-N60JCXZubcVyTUs02yaJjid7o2SBZ6-xtSXtmaiQE');
+
+let workSheet = [];
+
+app.get('/msgs', (req, res) => {
+  workSheet.getRows().then(rows => res.send(rows));
 });
 
+app.get('/msg', (req, res) => {
+  workSheet.addRow({ date: 'deez', message: 'nuts' })
+    .then(success => res.send('success'))
+    .catch(err => console.log(err));
+});
+
+mySheet.useServiceAccountAuth(creds).then(mySheet.getSpreadsheet.bind(mySheet)).then((sheet_info) => {
+  workSheet = sheet_info.worksheets[0];
+  app.listen(3000);
+});
